7f7835ac5bfa2de9ce36084d9298f04436e2230e
TameemMubarak <m.ansarithameem004@gmail.com>
2025-06-18 21:46:19 +0530

    Task-1 is updated .

--- Projects/Task-1/script.js
+++ Projects/Task-1/script.js
@@ -1,3 +1,24 @@
 function alertMessage(){
-    alert("hello! User ...")
+    alert("welcome to Sethi...")
 }
+
+function displayDate(){
+    const date=new Date();
+    const dateString=date.toLocaleString();
+    document.getElementById("date").textContent=dateString;
+}
+
+displayDate();
+setInterval(displayDate,1000);
+
+// function showDateTime() {
+//     const now = new Date();
+//     const dateTimeString = now.toLocaleString(); // formatted date and time
+//     document.getElementById("dateTimeDisplay").textContent = dateTimeString;
+//   }
+  
+//   // Show immediately
+//   showDateTime();
+  
+//   // Update every second (optional)
+//   setInterval(showDateTime, 1000);
